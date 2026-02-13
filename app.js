3365a001e040df19acd0fd9d04c01f0961babe45
Lucas Fraga Mendes <lucasfragamendes@gmail.com>
2026-02-12 21:18:35 -0300

    Add instruction (pick 1 to 10)

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 let title = document.querySelector('h1');
 title.innerHTML = 'Jogo Do Número Secreto';
+let instruction = document.querySelector ('p')
+instruction.innerHTML = 'Escolha um número entre 1 e 10'
