aacb3bee6d16b2e8b482db5bb93867457483d464
Lucas Fraga Mendes <lucasfragamendes@gmail.com>
2026-02-19 21:06:54 -0300

    Adding function and Generating secret number

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-let title = document.querySelector('h1');
-title.innerHTML = 'Jogo Do Número Secreto';
+//let title = document.querySelector('h1');
+//title.innerHTML = 'Jogo Do Número Secreto';
 
-let instruction = document.querySelector ('p');
-instruction.innerHTML = 'Escolha um número entre 1 e 10';
+//let instruction = document.querySelector ('p');
+//instruction.innerHTML = 'Escolha um número entre 1 e 10';
+
+
+let secretNumber = generateNumber ();
+
+function showTextInScreen (tag, text) {
+    let field = document.querySelector(tag);
+    field.innerHTML = text;
+}
+showTextInScreen('h1', 'Jogo do Número Secreto');
+showTextInScreen('p', 'Escolha um número entre 1 e 10');
 
 function verifyGuess() {
-    console.log ('o botão foi clicado');
+    console.log (secretNumber);
 
+}
+
+function generateNumber() {
+    return parseInt(Math.random() *10 + 1) ;
+    
 }
