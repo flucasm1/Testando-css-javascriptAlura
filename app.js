13319e9469bd8bbb3c5cab470f97b7e01b1ec549
Lucas Fraga Mendes <lucasfragamendes@gmail.com>
2026-02-25 21:32:26 -0300

    add console.log

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 let secretNumber = generateNumber ();
+console.log(secretNumber);
 
 function showTextInScreen (tag, text) {
     let field = document.querySelector(tag);
     field.innerHTML = text;
+    
 }
 showTextInScreen('h1', 'Jogo do Número Secreto');
 showTextInScreen('p', 'Escolha um número entre 1 e 10');
 
 function verifyGuess() {
     let guess = document.querySelector ('input').value;
+    
    
     if (guess == secretNumber) {
         showTextInScreen('h1', 'Acertou!');
